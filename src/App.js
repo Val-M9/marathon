b74b50165c0f31b3b7b1ca21b18c18454433a15d
Val_M 9 <v.mischenko90@gmail.com>
2021-01-29 13:30:23 +0200

    add urlBg colorBg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import bg from "./assets/bg3.jpg";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Layout from "./components/Layout/Layout";
@@ -10,12 +11,18 @@ const App = (props) => {
         title="Let's play the game"
         descr="Some kind of game, you can't win"
       />
-      <Layout id="1" title="First Stage" desc="Here will be rules" />
-      <Layout id="2" title="Second Stage" desc="And here toо" />
+      <Layout id="1" title="First Stage" desc="Here will be rules" urlBg={bg} />
+      <Layout
+        id="2"
+        title="Second Stage"
+        desc="And here too"
+        colorBg="yellow"
+      />
       <Layout
         id="3"
         title="The Last One"
         desc="No rules on the last stage ;)"
+        urlBg={bg}
       />
       <Footer />
     </div>
